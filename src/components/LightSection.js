c7aae0b038a92b6b80d5721f61df789bc0188102
David <davidjmoed@gmail.com>
2023-08-02 16:27:17 -0400

    Begin homepage light section

--- src/components/LightSection.js
+++ src/components/LightSection.js
@@ -1,8 +1,19 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 
-const LightSection = ({ img, heading, text, CTASection }) => {
-  return <Container className="LightSectionCont">LightSection</Container>;
+const LightSection = ({ img, header, text, CTASection }) => {
+  return (
+    <Row className="LightSectionCont">
+      <Col md={3} sm={12} className="LightSectionImgCont">
+        <img src={img} alt="Liikluskindlustus" />
+      </Col>
+      <Col md={9} sm={12} className="HomepageCTACont">
+        <h2>{header}</h2>
+        <p>{text}</p>
+        {CTASection}
+      </Col>
+    </Row>
+  );
 };
 
 export default LightSection;
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import DarkSection from "../components/DarkSection";
+import LightSection from "../components/LightSection";
 
 /* CTA sections */
 import SplashCTA from "../components/homepageCTAs/SplashCTA";
@@ -9,8 +10,8 @@ import "../styles/homepage.scss";
 
 const Homepage = () => {
   return (
-    <Container className="HomepageCont">
-      <div className="HomepageSectionCont HomepageSplashCont">
+    <div className="HomepageCont">
+      <Container className="HomepageSectionCont HomepageSplashCont">
         <DarkSection
           img={"https://placehold.co/444x444.png"}
           imgClass={"HomepageSplashImage"}
@@ -18,8 +19,17 @@ const Homepage = () => {
           text="Võrdle pakkumisi ja leia enda jaoks sobivaim kindlustus."
           CTASection={<SplashCTA />}
         />
+      </Container>
+      <div className="HomepageSectionCont HomepageAutoCont">
+        <LightSection
+          img={"https://placehold.co/720x450.png"}
+          header="Liikluskindlustus ja kasko"
+          text="Liikluskindlustus on Eestis kohustuslik kindlustus, millega hüvitatakse sinu sõidukiga teistele isikutele või nende varale põhjustatud kahjud.
+          Kaskokindlustus on autokindlustus, millega hüvitatakse Sinu sõidukile tekkinud kahju siis, kui põhjustad ise avarii või juhtub autoga mõni muu õnnetus."
+          CTASection={<SplashCTA />}
+        />
       </div>
-    </Container>
+    </div>
   );
 };
 
