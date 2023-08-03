9d36c6df0d30ea7f446abcbcbef9b0c8e5730b1b
David <davidjmoed@gmail.com>
2023-08-03 19:47:56 -0400

    improve styling and functionality for homepage auto section

--- Frontend/src/components/homepageCTAs/AutoCTA.js
+++ Frontend/src/components/homepageCTAs/AutoCTA.js
@@ -2,21 +2,40 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 const AutoCTA = () => {
-  const [CTAval, setCTAval] = useState("");
+  const [carRegistrationVal, setCarRegistrationVal] = useState("");
+  const [insuranceType, setInsuranceType] = useState("");
 
   return (
     <div className="AutoCTACont">
       <p className="AutoCTAHeader">sõiduki number</p>
       <input
         className="AutoCTAInput"
-        value={CTAval}
-        onChange={(e) => setCTAval(e.target.value)}
+        value={carRegistrationVal}
+        onChange={(e) => setCarRegistrationVal(e.target.value)}
       />
-      <p>*Väli ei ole kohustuslik</p>
+      <p>Väli ei ole kohustuslik</p>
       <div className="AutoCTAs">
-        <Button className="AutoButton">Liikluskindlustus</Button>
-        <Button className="AutoButton">Kaskokindlustus</Button>
-        <Button className="AutoButton">Liiklus + kaskokindlustus</Button>
+        <Button
+          className="AutoButton"
+          active={insuranceType === "Liikluskindlustus"}
+          onClick={() => setInsuranceType("Liikluskindlustus")}
+        >
+          Liikluskindlustus
+        </Button>
+        <Button
+          className="AutoButton"
+          active={insuranceType === "Kaskokindlustus"}
+          onClick={() => setInsuranceType("Kaskokindlustus")}
+        >
+          Kaskokindlustus
+        </Button>
+        <Button
+          className="AutoButton"
+          active={insuranceType === "Liiklus + kaskokindlustus"}
+          onClick={() => setInsuranceType("Liiklus + kaskokindlustus")}
+        >
+          Liiklus + kaskokindlustus
+        </Button>
       </div>
       <Button className="AutoClearButtonCTA">alusta kindlustamist</Button>
     </div>
@@ -24,3 +43,36 @@ const AutoCTA = () => {
 };
 
 export default AutoCTA;
+
+/*
+<label>
+          <input
+            className="AutoButton"
+            type="radio"
+            value="Liikluskindlustus"
+            checked={insuranceType === "Liikluskindlustus"}
+            onChange={() => setInsuranceType("Liikluskindlustus")}
+          />
+          Liikluskindlustus
+        </label>
+        <label>
+          <input
+            className="AutoButton"
+            type="radio"
+            value="Kaskokindlustus"
+            checked={insuranceType === "Kaskokindlustus"}
+            onChange={() => setInsuranceType("Kaskokindlustus")}
+          />
+          Kaskokindlustus
+        </label>
+        <label>
+          <input
+            className="AutoButton"
+            type="radio"
+            value="Liiklus + kaskokindlustus"
+            checked={insuranceType === "Liiklus + kaskokindlustus"}
+            onChange={() => setInsuranceType("Liiklus + kaskokindlustus")}
+          />
+          Liiklus + kaskokindlustus
+        </label>
+*/
